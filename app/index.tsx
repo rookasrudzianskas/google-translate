f6a755ec59ec8cda208134d3e5ca05594efbeeae
rokas <rokas@rokas.com>
2024-07-28 12:02:56 +0300

    Add language selection feature for translation

--- app/index.tsx
+++ app/index.tsx
@@ -2,35 +2,75 @@ import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
-import { Text, View, TextInput } from 'react-native';
+import {Text, View, TextInput, FlatList} from 'react-native';
 
 import AudioRecording from '~/components/AudioRecording';
 import { audioToText, textToSpeech, translate } from '~/utils/translation';
+import {languages} from "~/assets/languages";
 
 export default function Home() {
   const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
+  const [languageFrom, setLanguageFrom] = useState('English');
+  const [languageTo, setLanguageTo] = useState('Spanish');
+  const [selectLanguageMode, setSelectLanguageMode] = useState<'from' | 'to' | null>(null);
 
   const onTranslate = async () => {
-    const translation = await translate(input);
+    const translation = await translate(input, languageFrom, languageTo);
     setOutput(translation);
   };
 
   const speechToText = async (uri: string) => {
     const text = await audioToText(uri);
     setInput(text);
-    const translation = await translate(text);
+    const translation = await translate(text, languageFrom, languageTo);
     setOutput(translation);
   };
+
+  if (selectLanguageMode) {
+    return (
+      <FlatList
+        data={languages}
+        renderItem={({ item }) => (
+          <Text
+            onPress={() => {
+              if (selectLanguageMode === 'from') {
+                setLanguageFrom(item.name);
+              } else {
+                setLanguageTo(item.name);
+              }
+              setSelectLanguageMode(null);
+            }}
+            className="p-2 px-5">
+            {item.name}
+          </Text>
+        )}
+      />
+    );
+  }
 
   return (
     <View className="mx-auto w-full max-w-xl">
       <Stack.Screen options={{ title: 'Translate' }} />
       {/* Language selector row */}
       <View className="flex-row justify-around p-5">
-        <Text className="font-semibold color-blue-600">English</Text>
-        <FontAwesome5 name="exchange-alt" size={16} color="gray" />
-        <Text className="font-semibold color-blue-600">Spanish</Text>
+        <Text
+          onPress={() => setSelectLanguageMode('from')}
+          className="font-semibold color-blue-600">
+          {languageFrom}
+        </Text>
+        <FontAwesome5
+          onPress={() => {
+            setLanguageFrom(languageTo);
+            setLanguageTo(languageFrom);
+          }}
+          name="exchange-alt"
+          size={16}
+          color="gray"
+        />
+        <Text onPress={() => setSelectLanguageMode('to')} className="font-semibold color-blue-600">
+          {languageTo}
+        </Text>
       </View>
 
       {/* Input container */}
