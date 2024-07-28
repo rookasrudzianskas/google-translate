25ec81da720a4ed4d36f129d917371d83590408f
rokas <rokas@rokas.com>
2024-07-28 11:49:02 +0300

    Add CORS handling for preflight requests

--- supabase/functions/translate/index.ts
+++ supabase/functions/translate/index.ts
@@ -1,9 +1,15 @@
 import 'jsr:@supabase/functions-js/edge-runtime.d.ts';
 import OpenAI from 'npm:openai';
+import {corsHeaders} from "~/supabase/_shared/cors";
 
 const openai = new OpenAI();
 
 Deno.serve(async (req) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders });
+  }
+
   const { input, from, to } = await req.json();
 
   const completion = await openai.chat.completions.create({
@@ -20,7 +26,7 @@ Deno.serve(async (req) => {
   console.log(completion.choices[0]);
 
   return new Response(JSON.stringify(completion.choices[0].message), {
-    headers: { 'Content-Type': 'application/json' },
+    headers: { ...corsHeaders, 'Content-Type': 'application/json' },
   });
 });
 
